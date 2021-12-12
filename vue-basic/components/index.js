cc667f94ce099aa8641a8d68063d155eec94a4d6
Widdy Arfiansyah <widdyarfiansyah00@gmail.com>
2021-12-12 11:02:33 +0700

    Basic Vue JS - Finish

---
+++ vue-basic/components/index.js
@@ -0,0 +1,45 @@
+Vue.component('header-component', {
+    data() {
+        return {
+            firstData: 'Widdy Arfiansyah',
+        }
+    },
+    template: ` <header>
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light border">
+                        <a class="navbar-brand" href="#">{{ firstData }}</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link class="nav-link" :to="{ name: 'create' }">Tambah Barang</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <slot></slot>
+                </header>`
+});
+
+Vue.component('footer-component', {
+    template: ` <footer>
+                    <p>&copy; 2021 By Widdy Arfiansyah</p>
+                </footer>`,
+});
+
+Vue.component('cart-component', {
+    props: ['total', 'carts', 'item'],
+    template: ` <div>
+                    <h3>Keranjang Anda</h3>
+                    <ul>
+                        <li v-for="cart in carts" :key="cart.id">
+                            Barang : {{ cart.title }} | Price : {{ '$' + cart.price }} | Total : {{ cart.qty }}
+                        </li>
+                    </ul>
+                    <p>Total Item {{ item }} <br> Total Harga {{ '$' + total }}</p>
+                </div>`
+});
---
+++ vue-basic/router/index.js
@@ -0,0 +1,103 @@
+const Form = {
+    props: ['barang'],
+    template: ` <div>
+                    <form @submit.prevent="$emit('add-barang')" class="pb-3">
+                        <div class="row pb-3">
+                            <div class="col-6">
+                                <input type="text" placeholder="Nama Barang" v-model="barang.title" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row pb-3">
+                            <div class="col-6">
+                                <input type="number" placeholder="Harga Barang" v-model="barang.price" class="form-control" required>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Tambah</button>
+                    </form>
+                </div>`
+}
+
+const Home = {
+    props: ['total', 'carts', 'item', 'barang', 'loading', 'errors'],
+    methods: {
+        showImage(pathImage) {
+            return pathImage ?? 'assets/images/no-image.png';
+        },
+    },
+    template: ` <div>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Photo</th>
+                                <th>Barang</th>
+                                <th>Harga</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <template v-if="loading">
+                                <tr><td colspan="5" :class="{ loads: loading }">Loading </td></tr>
+                            </template>
+                            <template v-else>
+                                <template v-if="errors.status == false">
+                                    <template v-if="barang.length > 0">
+                                        <tr v-for="(brg,i) in barang" :key="brg.id">
+                                            <td>{{ i + 1 }}</td>
+                                            <td>
+                                                <img :src="showImage(brg.image)" width="50">
+                                            </td>
+                                            <td>{{ brg.title }}</td>
+                                            <td>{{ '$' + brg.price }}</td>
+                                            <td>
+                                                <button @click="$emit('delete-barang', brg.id,i)" class="btn btn-danger btn-sm">Delete</button>
+                                                |
+                                                <button @click="$emit('add-to-cart', brg.id)" class="btn btn-secondary btn-sm">+</button>
+                                                <button @click="$emit('remove-from-cart', brg.id)" class="btn btn-secondary btn-sm">-</button>
+                                            </td>
+                                        </tr>
+                                    </template>
+                                    <template v-else>
+                                            <tr><td colspan="5">Data Not Found</td></tr>
+                                    </template>
+                                </template>
+                                <template v-else>
+                                    <tr><td colspan="5">{{ errors.message }}</td></tr>
+                                </template>
+                            </template>
+                        </tbody>
+                    </table>
+                    <div>
+                        <h3>Keranjang Anda</h3>
+                        <ul>
+                            <li v-for="cart in carts" :key="cart.id">
+                                Barang : {{ cart.title }} | Price : {{ '$' + cart.price }} | Total : {{ cart.qty }}
+                            </li>
+                        </ul>
+                        <p>Total Item {{ item }} <br> Total Harga {{ '$' + total }}</p>
+                    </div>
+                </div>`
+}
+
+const NotFound = {
+    template: `<h3>404 NOT FOUND</h3>`
+}
+
+const router = new VueRouter({
+    linkClassActive: 'active',
+    routes: [{
+            path: '/',
+            component: Home,
+            name: 'home'
+        },
+        {
+            path: '/create',
+            component: Form,
+            name: 'create'
+        },
+        {
+            path: '*',
+            component: NotFound
+        }
+    ]
+});
